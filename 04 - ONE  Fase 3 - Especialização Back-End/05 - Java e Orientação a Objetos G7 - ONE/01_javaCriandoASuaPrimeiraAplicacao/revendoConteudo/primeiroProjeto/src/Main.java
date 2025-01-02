41be44059a938a0e32f31d21fe55a9932b0e6c3f
jeffersonsouzasilva <92387696+jeffersonsouzasilva@users.noreply.github.com>
2025-01-01 22:18:15 -0300

    Atribuindo valores ao filme -- Revendo Conteudo -- Java: criando a sua primeira aplicação

--- 04 - ONE  Fase 3 - Especialização Back-End/05 - Java e Orientação a Objetos G7 - ONE/01_javaCriandoASuaPrimeiraAplicacao/revendoConteudo/primeiroProjeto/src/Main.java
+++ 04 - ONE  Fase 3 - Especialização Back-End/05 - Java e Orientação a Objetos G7 - ONE/01_javaCriandoASuaPrimeiraAplicacao/revendoConteudo/primeiroProjeto/src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Esse é o Screen Match");
+        System.out.println("Filme: Top Gun: Maverick");
+
+        int anoDelancamento = 2022;
+        System.out.println("Ano de lançamento: " + anoDelancamento);
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 8.1;
+
     }
 }
